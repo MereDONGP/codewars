b589bd9a1b1f675929ce3ae0f62e8aa900b25f8b
Don <pullend15@gmail.com>
2021-06-26 18:48:13 -0400

    adding in teh counting duplicates

---
+++ countingduplicates.js
@@ -0,0 +1,22 @@
+function duplicateCount(text){
+    let matches = {}
+    let count = 0
+    
+    for (let i = 0; i < text.length; i++){
+      let lowers = text[i].toLowerCase()
+      if(lowers in matches){
+        matches[lowers] += 1
+      }
+      else{
+        matches[lowers] = 1
+      }
+    }
+    
+    for (i in matches){
+        if (matches[i] >= 2){
+          count += 1
+      }
+    }
+    
+    return count
+  }
